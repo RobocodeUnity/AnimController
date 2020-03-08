2441dfaa90937c5b456714afd0e2960e8c736279
Oksanny <oksana.voitko7@gmail.com>
2020-03-08 14:12:55 +0200

    Initial commit

---
+++ Assets/CubeMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CubeMover : MonoBehaviour
+{
+    public Rigidbody rb;
+    public NavMeshAgent navMesh;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Debug.Log(navMesh.velocity.magnitude);
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray nashRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(nashRay, out hitInfo))
+            {
+                navMesh.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
---
+++ Assets/Kevin Iglesias/Basic Motions Pack/Models/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(myRay, out hitInfo))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+        
+    }
+   
+}
